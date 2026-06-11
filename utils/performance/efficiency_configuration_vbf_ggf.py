2124bad8cc68b6f70af63d96f810599ba62414ca
Matteo Malucchi <matteomalucchi13@gmail.com>
2026-06-11 09:34:13 +0200

    Update efficiency configuration scripts to include new dataset entries and modify existing ones for VBF pairing and classification

--- utils/performance/efficiency_configuration_vbf_ggf.py
+++ utils/performance/efficiency_configuration_vbf_ggf.py
@@ -47,27 +47,27 @@ spanet_dict = {
         'color': 'orange',
         'vbf': True,
     },
-    'hh4b_pairing_vbf_ggf_pairing_classification_allKalmbda': {
-        'file': f'{new_spanet_dir_matteo}/out_hh4b_pairing_vbf_ggf_pairing_classification_allKlambda/out_seed_trainings_100/version_2/JetTotalSPANetPadded_kl_combined_EVENT_AllKlambda_classification_ptvarytraining_reverse_test.h5',
-        'true': '9_jets_vbf_ggf_all_Klambda',
-        'label': 'SPANet - VBF/ggF - pairing+classification - 9 jets',
-        'color': 'blue',
-        'vbf': True,
-    },
-    'hh4b_pairing_vbf_ggf_pairing_classification_allKalmbda_7jets_100e': {
-        'file': f'{new_spanet_dir_matteo}/out_hh4b_pairing_vbf_ggf_pairing_classification_allKlambda_7jets/out_seed_trainings_100/version_1/predict_7jets_100e_JetTotalSPANetPadded_test.h5',
-        'true': '7_jets_vbf_ggf_all_Klambda',
-        'label': 'SPANet - VBF/ggF - pairing+classification - 7 jets - 100 epochs',
-        'color': 'dodgerblue',
-        'vbf': True,
-    },
-    'hh4b_pairing_vbf_ggf_pairing_classification_allKalmbda_7jets_200e': {
-        'file': f'{new_spanet_dir_matteo}/out_hh4b_pairing_vbf_ggf_pairing_classification_allKlambda_7jets/out_seed_trainings_100/version_0/predict_7jets_JetTotalSPANetPadded_test.h5',
-        'true': '7_jets_vbf_ggf_all_Klambda',
-        'label': 'SPANet - VBF/ggF - pairing+classification - 7 jets - 200 epochs',
-        'color': 'red',
-        'vbf': True,
-    },
+    # 'hh4b_pairing_vbf_ggf_pairing_classification_allKalmbda': {
+    #     'file': f'{new_spanet_dir_matteo}/out_hh4b_pairing_vbf_ggf_pairing_classification_allKlambda/out_seed_trainings_100/version_2/JetTotalSPANetPadded_kl_combined_EVENT_AllKlambda_classification_ptvarytraining_reverse_test.h5',
+    #     'true': '9_jets_vbf_ggf_all_Klambda',
+    #     'label': 'SPANet - VBF/ggF - pairing+classification - 9 jets',
+    #     'color': 'blue',
+    #     'vbf': True,
+    # },
+    # 'hh4b_pairing_vbf_ggf_pairing_classification_allKalmbda_7jets_100e': {
+    #     'file': f'{new_spanet_dir_matteo}/out_hh4b_pairing_vbf_ggf_pairing_classification_allKlambda_7jets/out_seed_trainings_100/version_1/predict_7jets_100e_JetTotalSPANetPadded_test.h5',
+    #     'true': '7_jets_vbf_ggf_all_Klambda',
+    #     'label': 'SPANet - VBF/ggF - pairing+classification - 7 jets - 100 epochs',
+    #     'color': 'dodgerblue',
+    #     'vbf': True,
+    # },
+    # 'hh4b_pairing_vbf_ggf_pairing_classification_allKalmbda_7jets_200e': {
+    #     'file': f'{new_spanet_dir_matteo}/out_hh4b_pairing_vbf_ggf_pairing_classification_allKlambda_7jets/out_seed_trainings_100/version_0/predict_7jets_JetTotalSPANetPadded_test.h5',
+    #     'true': '7_jets_vbf_ggf_all_Klambda',
+    #     'label': 'SPANet - VBF/ggF - pairing+classification - 7 jets - 200 epochs',
+    #     'color': 'red',
+    #     'vbf': True,
+    # },
     ##'hh4b_pairing_vbf_ggf_pairing_allKalmbda': {
     ##    'file': f'{new_spanet_dir_matteo}/out_hh4b_pairing_vbf_ggf_pairing_classification_allKlambda/out_seed_trainings_100/version_1/JetTotalSPANetPadded_kl_combined_test_vbf_all_Klambda_predicitons.h5',
     ##    'true': '9_jets_vbf_ggf_all_Klambda',
@@ -89,7 +89,17 @@ spanet_dict = {
     #     'color': 'green',
     #     'vbf': True,
     # },
-    # ============================================= DATA ===========================================================
+    ## 
+    'hh4b_pairing_vbf_ggf_all_Klambda_VBFPairing_JetVBFHiggs_DNNVars': {
+        'file': f'{new_spanet_dir_matteo}/out_hh4b_pairing_vbf_ggf_all_Klambda_VBFPairing_JetVBFHiggs_DNNVars/out_seed_trainings_100/version_0/predict_FixMASK_AllKlambda_VBFggF_VBFPairingAfterHiggsPairing_DNNVars_JetGoodVBFMergedProvVBFPadded_JetGoodProvHiggsPadded_test.h5',
+        'true': '9jets_all_Klambda_VBFPairing_JetVBFHiggs_DNNVars',
+        'label': 'SPANet - VBF pairing+classification - DNN Vars',
+        'color': 'blue',
+        'vbf': True,
+        'jet_coll': 'JetVBF',
+        'n_higgs_jets': 0,
+        'offset_jet_idx': -4,
+    },
 }
 
 
@@ -120,93 +130,11 @@ true_dict = {
         "name": f"{new_true_dir_matteo}/vbf/vbf_ptFlattenMatchedHiggs_all_Klambda_DetaMjj_SeparateHiggsVBF/AllKlambda_DetaMjj_SeparateHiggsVBF_JetGoodProvHiggsPadded_JetGoodVBFMergedProvVBFPadded_test.h5",
         "klambda": "postEE",
     },
+    "9jets_all_Klambda_VBFPairing_JetVBFHiggs_DNNVars": {
+        "name": f'{new_true_dir_matteo}/vbf/vbf_ggf_all_Klambda_DetaMjjCentrality_VBFPairingAfterHiggsPairing_DNNVars/FixMASK_AllKlambda_VBFggF_VBFPairingAfterHiggsPairing_DNNVars_JetGoodVBFMergedProvVBFPadded_JetGoodProvHiggsPadded_test.h5',
+        "klambda": "postEE",
+        'jet_coll': 'JetVBF',
+        'n_higgs_jets': 0
+    },
 }
 
-
-# =============================================================================
-# (nkontaxa) MODIFICATIONS TO spanet_dict below
-# =============================================================================
-spanet_dir_nestor = "/eos/user/n/nkontaxa/semester_project/spanet_outputs/"
-true_dir_nestor = "/eos/user/m/mmalucch/spanet_infos/spanet_inputs/"
-
-spanet_dict.update({
-    ###AllKlambda 7 jets
-    ##f"{spanet_dir_nestor}vbf/predictions_allKlambda_7jets_JetTotalSPANETPadded.h5": {
-    ##    "file": f"{spanet_dir_nestor}vbf/predictions_allKlambda_7jets_JetTotalSPANETPadded.h5",
-    ##    "true": "vbf_jet_total_padded",
-    ##    "label": "SPANet 7 jets all_Kl_PtFlattened_training (nkontaxa)",
-    ##    "color": "royalblue",
-    ##    "vbf": True,
-    ##},
-    
-    #AllKlambda 9 jets
-    ##f"{spanet_dir_nestor}vbf/predictions_allKlambda.h5": {
-    ##    "file": f"{spanet_dir_nestor}vbf/predictions_allKlambda.h5",
-    ##    "true": "vbf_jet_total_padded",
-    ##    "label": "SPANet 9 jets all_Klambda (nkontaxa)",
-    ##    "color": "red",
-    ##    "vbf": True,
-    ##},
-
-    #500 epochs all_Klambda 9 jets
-    f"{spanet_dir_nestor}vbf/predictions_allKlambda_500epochs.h5": {
-        "file": f"{spanet_dir_nestor}vbf/predictions_allKlambda_500epochs.h5",
-        "true": "vbf_jet_total_padded",
-        "label": "SPANet 9 jets all_Klambda 500 epochs (nkontaxa)",
-        "color": "blue",
-        "vbf": True,
-    },
-    
-    #allKlambda_DetaMjj_NGW_noextravars
-    f"{spanet_dir_nestor}vbf/predictions_allKlambda_DetaMjj_novars_NormGenWeights.h5": {
-        "file": f"{spanet_dir_nestor}vbf/predictions_allKlambda_DetaMjj_novars_NormGenWeights.h5",
-        "true": "vbf_jet_total_padded",
-        "label": "SPANet DetaMjj NGW noextravars (nkontaxa)",
-        "color": "black",
-        "vbf": True,
-    },
-
-    #allKlambda_DetaMjj_NNGW_noextravars
-        f"{spanet_dir_nestor}vbf/predictions_allKlambda_DetaMjj_novars_Non-NormGenWeights.h5": {
-            "file": f"{spanet_dir_nestor}vbf/predictions_allKlambda_DetaMjj_novars_Non-NormGenWeights.h5",
-            "true": "vbf_jet_total_padded",
-            "label": "SPANet DetaMjj NNGW noextravars (nkontaxa)",
-            "color": "green",
-            "vbf": True,
-        },
-
-})
-
-true_dict.update({
-    #AllKlambda 7 jets
-    "vbf_jet_total_padded": {
-        "name": f"{true_dir_nestor}vbf/vbf_all_Klambda/7jets_JetTotalSPANetPadded_test.h5",
-        "klambda": "postEE",
-        "vbf": True,
-    },
-    
-    #AllKlambda 9 jets
-    "vbf_jet_total_padded": {
-        "name": f"{true_dir_nestor}vbf/vbf_all_Klambda/JetTotalSPANetPadded_kl_combined_EVENT_test.h5",
-        "klambda": "postEE",
-        "vbf": True,
-    },
-
-    #AllKlambda DetaMjj NNGW noextravars jets
-    "vbf_jet_total_padded": {
-        "name": f"{true_dir_nestor}vbf/vbf_all_Klambda_DetaMjj/SaveMjjDeta_JetTotalSPANetPadded_test.h5",
-        "klambda": "postEE",
-        "vbf": True,
-    },
-
-    #AllKlambda DetaMjj NGW noextravars jets
-    "vbf_jet_total_padded": {
-        "name": f"{true_dir_nestor}vbf/vbf_all_Klambda_DetaMjj/SaveMjjDeta_NormGenWeights_JetTotalSPANetPadded_test.h5",
-        "klambda": "postEE",
-        "vbf": True,
-    },
-    # Add more entries here...
-})
-# =============================================================================
-# END MODIFICATIONS SECTION
-# =============================================================================
