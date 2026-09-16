78d1b7b4ad77b12ab894c539c032f9d030e0d84e
Matteo Malucchi <matteomalucchi13@gmail.com>
2026-09-16 10:53:09 +0200

    Add new VBF/ggF dataset configuration and update jet collections handling

--- utils/dataset/collections_coffea_to_h5_direct.py
+++ utils/dataset/collections_coffea_to_h5_direct.py
@@ -114,15 +114,15 @@ jet_collections_dict = {
         },
     ],
     "JET_COLLECTIONS_VBF_PAIRING_AFTER_HIGGS_PAIRING_TOTAL": [
-        {
-            "JetGoodVBFMergedProvVBFPadded": {
-                "saved_name": "Jet",
-                "max_num_jets": 5,
-                "resonances": ["vbf"],
-                "min_num_jets": 0,
-                "prov_key": "provenance_vbf",
-            },
-        },
+        # {
+        #     "JetGoodVBFMergedProvVBFPadded": {
+        #         "saved_name": "Jet",
+        #         "max_num_jets": 5,
+        #         "resonances": ["vbf"],
+        #         "min_num_jets": 0,
+        #         "prov_key": "provenance_vbf",
+        #     },
+        # },
         {
             "JetGoodVBFMergedProvVBFPadded": {
                 "saved_name": "JetVBF",
@@ -139,15 +139,15 @@ jet_collections_dict = {
                 "prov_key": "provenance_higgs",
             },
         },
-        {
-            "JetTotalSPANetPadded": {
-                "saved_name": "Jet",
-                "max_num_jets": 9,
-                "resonances": ["vbf"],
-                "min_num_jets": 4,
-                "prov_key": "provenance_vbf",
-            },
-        },
+        # {
+        #     "JetTotalSPANetPadded": {
+        #         "saved_name": "Jet",
+        #         "max_num_jets": 9,
+        #         "resonances": ["vbf"],
+        #         "min_num_jets": 4,
+        #         "prov_key": "provenance_vbf",
+        #     },
+        # },
     ],
     "JET_COLLECTION_VBF_BOOSTED": [
         {
